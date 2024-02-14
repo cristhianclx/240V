7072b938496d78f60b00d4ba77fef9ca340d54cb
Cristhian <cristhianclx@gmail.com>
2024-02-13 21:23:21 -0500

    update details

--- 01/project/web/admin.py
+++ 01/project/web/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, ReviewDetail
 
 admin.site.register(Review)
+admin.site.register(ReviewDetail)
--- 01/project/web/models.py
+++ 01/project/web/models.py
@@ -15,3 +15,16 @@ class Review(models.Model):
         verbose_name="Rating (1-5)",
         blank=False,
     )
+
+
+class ReviewDetail(models.Model):
+    review = models.ForeignKey(
+        Review,
+        verbose_name="Review",
+        blank=False,
+        on_delete=models.CASCADE,
+    )
+    detail = models.TextField(
+        verbose_name="Detail",
+        blank=False,
+    )
