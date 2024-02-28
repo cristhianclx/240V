bf2d34755a9e3697dd891bc2c184d38526673e39
Cristhian <cristhianclx@gmail.com>
2024-02-27 19:56:53 -0500

    update pdf support

--- 03/project/web/utils.py
+++ 03/project/web/utils.py
@@ -1,9 +1,11 @@
+import magic
 import docx
 import re
 import os
 import nltk
 import pickle
 import pandas as pd
+from pypdf import PdfReader
 from nltk.corpus import stopwords
 from django.conf import settings
 
@@ -22,12 +24,23 @@ ranker = pd.read_pickle(ranker_file)
 
 
 def parseFileInformation(filename):
-    doc = docx.Document(filename)
-    fullText = []
-    for para in doc.paragraphs:
-        fullText.append(para.text)
-    return "\n".join(fullText)
-
+    mimetype = magic.from_file(filename.name, mime=True)
+    if mimetype == "application/pdf":
+        reader = PdfReader(filename.name)
+        number_of_pages = len(reader.pages)
+        fullText = ""
+        for page in range(0, number_of_pages):
+            page = reader.pages[page]
+            text = page.extract_text()
+            fullText = fullText + text
+        return fullText
+    if mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        doc = docx.Document(filename)
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        return "\n".join(fullText)
+    return None
 
 def wrangler(raw):
     letras = re.sub("[^a-zA-ZáóéíúñÑ]", " ", raw)
